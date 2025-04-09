2034c9cff7b61ad5f9045241e5e93989d45a5d15
wayne-bai-ics4u1-02-25 <wayne.bai@student.tdsb.on.ca>
2025-04-09 14:16:08 +0000

    geweowejoigwjiogrjoigjoigejioge;jogejio; ej;oi ej io; trj;  ht;o t; tr tr; r jr n r r  en r e jtroi j ro;orj orj, hli jerl htrn eroiorthort yep

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,8 +4,23 @@ public class PracticeProblem {
 
 	}
 
-	public static void 
+	public static int sumOfDigits(int x) {
+		x = Math.abs(x);
+		if (x < 10) {
+			return x;
+		}
+		return x % 10 + sumOfDigits(x / 10);
+	}
 
+	public static int factorial(int x) {
+		if (x == 0) {
+			return 1;
+		}
+		if (x < 0) {
+			return 0;
+		}
+		return x * factorial(x-1);
+	}
 	
 
 	
